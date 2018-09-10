614a7aa9b714f14bb7ca40bd99634d9aa598ec39
fidzilalluqman <42725339+fidzilalluqman@users.noreply.github.com>
2018-09-10 14:14:44 +0000

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,31 @@
 <?php
-
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
+function rekursifBintang($p) {
+  $p--;
+  if ($p > 0)rekursifBintang($p);
+	echo "*";
 }
-rekursif(5);
+function rekursifBintang2($p){
+  rekursifBintang($p);
+  echo "<br>";
+  $p--;
+  if($p>0)
+    rekursifBintang2($p);
+}
+	rekursifBintang2(6);
+
+echo "<br>=================================<br><br>";
+
+function rekursifAngka($q, $r){
+  echo $q;
+    $q++;
+  		if($q <=$r)rekursifAngka($q, $r);  
+}
+function rekursifAngka2($q, $r){
+  rekursifAngka(1, $q);
+   echo "<br>";
+  	$q++;
+  		if($q <=$r)rekursifAngka2($q, $r);  
+}
+rekursifAngka2(1,6);
+
 ?>
